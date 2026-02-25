6c806c94b22fbe476adac9abd0a57cdd4894cc3b
yur1k14 <Yurii.Poliuha.ShI.2023@lpnu.ua>
2026-02-25 11:20:41 +0200

    Create new branch feature/data_load and add script for downloading dataset

---
+++ src/data_load.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+
+def load_data(FILE_PATH):
+    if not os.path.exists(FILE_PATH):
+        print("Помилка: Файл не знайдено за шляхом {}".format(FILE_PATH))
+        return None
+
+    try:
+        df = pd.read_excel(FILE_PATH, engine = "xlrd")
+        print("Вивід інформації про датасет:")
+        print("\nЗагальна інформація про датасет:")
+        print(df.shape)
+        print("\nІнформація про колонки:")
+        print(df.info())
+        print("\nПерші 5 рядків:")
+        print(df.head())
+        print("\nОстанні 5 рядків:")
+        print(df.tail())
+
+        return df
+
+    except Exception as e:
+        print("Помилка при зчитуванні файлу {}".format(e))
+        return None
+
+if __name__ == "__main__":
+    FILE_PATH = "../data/raw/dataset.xls"
+    load_data(FILE_PATH)
