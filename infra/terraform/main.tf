fbf4345bae96d6a5426d6bdce0ad9e91743e2754
yur1k14 <Yurii.Poliuha.ShI.2023@lpnu.ua>
2026-05-28 08:43:03 +0300

    feat: add monitoring infrastructure configurations for Prometheus and Grafana

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -70,6 +70,30 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+security_rule {
+    name                       = "Prometheus"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Grafana"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # 6. Створення Мережевого інтерфейсу (NIC)
